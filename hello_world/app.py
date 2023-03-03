de595767b051324c000cbbc8f22ac869214dfd71
Ahmad Siddiqi <ahmad.siddiqi158@gmail.com>
2023-03-03 16:58:20 -0500

    testing pipeline

--- hello_world/app.py
+++ hello_world/app.py
@@ -6,7 +6,7 @@ table = ddb.Table('Emergency')
 def lambda_handler(event, context):
     #Adding the emergency to the DynamoDB table
     table.put_item(Item=event)
-    print("PIPELINE_ADDED")
+    
     #Get all the emergencies that have been reported
     emergencies = table.scan()['Items']
 
